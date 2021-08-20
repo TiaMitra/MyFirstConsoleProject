cc05c144e3cf468eaa3aa1545981cdf0ec4a4808
TiaMitra <89232194+TiaMitra@users.noreply.github.com>
2021-08-19 21:43:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name="Tiger";
+var last_name="Shroff";
+console.log(first_name);
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=20;
+var b=70;
+var mul=a*b;
+console.log(mul);
+var div=a/b;
+console.log(div);
